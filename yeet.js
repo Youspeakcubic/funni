582bd99eec56f9eb916e6cb3470315615b89bc17
Anthony <delantho327@gmail.com>
2022-01-16 18:10:25 -0600

    init commit

---
+++ yeet.js
@@ -0,0 +1,7 @@
+
+let trin = () => {
+document.getElementById('bodyThing').style.display="none";
+document.getElementById('ames').style.display="block";
+document.getElementById('sanImg').className = "sanImg sanImgAnim";
+document.getElementById('text').className="sanImgAnim2";
+}
